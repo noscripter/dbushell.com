7e4d8197debf6cc5dcfecc214bc3ccbf1732bd57
David Bushell <david@dbushell.com>
2016-02-05 17:42:30 +0000

    work in progress

--- src-assets/js/main.js
+++ src-assets/js/main.js
@@ -184,168 +184,198 @@ if (/dbushell\.com/.test(window.location.hostname))
                 return;
             }
 
-            var mouse = { },
+            var $nav = document.getElementById('nav');
 
-                nav_open = false,
-                nav_mouse = false,
-
-                scroll_out,
-                scroll_delta,
-                scroll_dist = 0,
-                scroll_down = false,
-                scroll_top  = [0, 0],
-                win_width   = 0,
-                win_height  = 0;
-
-            var $header       = document.getElementById('top'),
-                header_height = 0,
-                header_width  = 0;
-
-            app.onResize = function(e)
+            function updateNav()
             {
-                app.onScroll(e);
-            };
 
-            app.onScroll = function(e)
-            {
-                mouse = { };
-
-                scroll_top[0] = scroll_top[1];
-                scroll_top[1] = window.pageYOffset || document.documentElement.scrollTop;
-
-                scroll_down  = scroll_top[1] > scroll_top[0];
-                scroll_delta = scroll_top[1] - scroll_top[0];
-
-                if (scroll_top[1] > header_height) {
-                    if (scroll_down) {
-                        scroll_dist = 0;
-                        addClass($header, 'header--fixed');
-                        addClass($header, 'header--inactive');
-                    } else {
-                        scroll_dist += scroll_delta;
-                        if (scroll_dist < -10) {
-                            removeClass($header, 'header--inactive');
-                        }
-                    }
+                if ($nav.scrollLeft > 10) {
+                    $nav.classList.add('nav--left-overflow');
                 } else {
-                    removeClass($header, 'header--fixed');
-                    removeClass($header, 'header--inactive');
+                    $nav.classList.remove('nav--left-overflow');
                 }
-
-                clearTimeout(scroll_out);
-                if (e) {
-                    scroll_out = setTimeout(function() {
-                        if ((window.pageYOffset || document.documentElement.scrollTop) <= 0) {
-                            removeClass($header, 'header--fixed');
-                            removeClass($header, 'header--inactive');
-                        }
-                    }, 300);
-                }
-            };
-
-            addEvent(window, 'scroll', _throttle(app.onScroll, 100), false);
-            addEvent(window, 'resize', _throttle(app.onResize, 100), false);
-            addEvent(window, 'orientationchange', _throttle(app.onResize, 100), false);
-
-            app.onResize();
-            app.onScroll();
-
-            addEvent(window, 'mousemove', _throttle(function(e)
-            {
-                if (nav_open) { return; }
-                var pos;
-                if (e.hasOwnProperty('pageX')) {
-                    pos = [e.pageX, e.pageY];
+                if ($nav.scrollWidth - $nav.clientWidth - $nav.scrollLeft > 10) {
+                    $nav.classList.add('nav--right-overflow');
                 } else {
-                    pos = [e.clientX + document.documentElement.scrollLeft, e.clientY + document.documentElement.scrollTop];
+                    $nav.classList.remove('nav--right-overflow');
                 }
-                if (!mouse.pos) {
-                    mouse.pos = pos;
-                    return;
-                }
-                mouse.angle = Math.atan2(pos[1] - mouse.pos[1], pos[0] - mouse.pos[0]) * 180 / Math.PI;
-                mouse.pos = pos;
+            }
 
-            }, 100), false);
+            addEvent(window, 'load', updateNav, false);
+            addEvent(window, 'resize', updateNav, false);
+            addEvent($nav, 'scroll', updateNav, false);
 
-            var nav_onMouseLeave = function(e)
-            {
-                app.closeNav();
-            };
+            var headroom  = new Headroom($nav, { offset: 35 });
+            headroom.init();
 
-            addEvent($header, 'mouseenter', function(e)
-            {
-                if (nav_open || typeof mouse.angle !== 'number') {
-                    return;
-                }
-                if (e.hasOwnProperty('pageX')) {
-                    pos = [e.pageX, e.pageY];
-                } else {
-                    pos = [e.clientX + document.documentElement.scrollLeft, e.clientY + document.documentElement.scrollTop];
-                }
-                if (mouse.angle > -180 && mouse.angle < -90) {
-                    if (pos[0] < 160) {
-                        nav_open = true;
-                        nav_mouse = false;
-                        setTimeout(function() {
-                            addEvent($nav, 'mouseleave', nav_onMouseLeave, false);
-                        }, 150);
-                        app.openNav();
-                    }
-                }
-            },
-            false);
+            // document.querySelector('div').onscroll = function() {
+            //     this.classList[this.scrollTop < 20 ? 'add' : 'remove']('shadow-top');
+            //     this.classList[this.scrollHeight - this.clientHeight - this.scrollTop < 20 ? 'add' : 'remove']('shadow-bottom');
+            // };
 
-            var $nav     = document.getElementById('nav'),
-                $overlay = document.getElementById('overlay'),
-                $main    = document.getElementsByTagName('main')[0];
+            // var mouse = { },
 
-            app.openNav = function()
-            {
-                nav_open = true;
-                addClass($nav, 'nav--active');
-                removeClass($nav, 'nav--closed');
-                removeClass($nav, 'nav--hidden');
-                addClass($overlay, 'overlay--active');
-            };
+            //     nav_open = false,
+            //     nav_mouse = false,
 
-            app.closeNav = function()
-            {
-                mouse = { };
-                nav_open = false;
-                addClass($nav, 'nav--closed');
-                removeClass($nav, 'nav--active');
-                removeClass($overlay, 'overlay--active');
-                removeEvent($nav, 'mouseleave', nav_onMouseLeave, false);
-                setTimeout(function() {
-                    addClass($nav, 'nav--hidden');
-                }, 300);
-            };
+            //     scroll_out,
+            //     scroll_delta,
+            //     scroll_dist = 0,
+            //     scroll_down = false,
+            //     scroll_top  = [0, 0],
+            //     win_width   = 0,
+            //     win_height  = 0;
 
-            addEvent(document.getElementById('nav-open'), 'click', function(e)
-            {
-                if (e.preventDefault) e.preventDefault(); else e.returnValue = false;
-                app.openNav();
-            },
-            false);
+            // var $header       = document.getElementById('top'),
+            //     header_height = 0,
+            //     header_width  = 0;
 
-            addEvent(document.getElementById('nav-close'), 'click', function(e)
-            {
-                if (e.preventDefault) e.preventDefault(); else e.returnValue = false;
-                app.closeNav();
-            },
-            false);
+            // app.onResize = function(e)
+            // {
+            //     app.onScroll(e);
+            // };
 
-            addEvent(document.getElementById('overlay'), 'click', app.closeNav, false);
+            // app.onScroll = function(e)
+            // {
+            //     mouse = { };
 
-            addEvent(window, 'keydown', function(e)
-            {
-                if (e.which === 27) {
-                    setTimeout(function() {
-                        if (nav_open) app.closeNav();
-                    }, 50);
-                }
-            }, false);
+            //     scroll_top[0] = scroll_top[1];
+            //     scroll_top[1] = window.pageYOffset || document.documentElement.scrollTop;
+
+            //     scroll_down  = scroll_top[1] > scroll_top[0];
+            //     scroll_delta = scroll_top[1] - scroll_top[0];
+
+            //     if (scroll_top[1] > header_height) {
+            //         if (scroll_down) {
+            //             scroll_dist = 0;
+            //             addClass($header, 'header--fixed');
+            //             addClass($header, 'header--inactive');
+            //         } else {
+            //             scroll_dist += scroll_delta;
+            //             if (scroll_dist < -10) {
+            //                 removeClass($header, 'header--inactive');
+            //             }
+            //         }
+            //     } else {
+            //         removeClass($header, 'header--fixed');
+            //         removeClass($header, 'header--inactive');
+            //     }
+
+            //     clearTimeout(scroll_out);
+            //     if (e) {
+            //         scroll_out = setTimeout(function() {
+            //             if ((window.pageYOffset || document.documentElement.scrollTop) <= 0) {
+            //                 removeClass($header, 'header--fixed');
+            //                 removeClass($header, 'header--inactive');
+            //             }
+            //         }, 300);
+            //     }
+            // };
+
+
+            // addEvent(window, 'scroll', _throttle(app.onScroll, 100), false);
+            // addEvent(window, 'resize', _throttle(app.onResize, 100), false);
+            // addEvent(window, 'orientationchange', _throttle(app.onResize, 100), false);
+
+            // app.onResize();
+            // app.onScroll();
+
+            // addEvent(window, 'mousemove', _throttle(function(e)
+            // {
+            //     if (nav_open) { return; }
+            //     var pos;
+            //     if (e.hasOwnProperty('pageX')) {
+            //         pos = [e.pageX, e.pageY];
+            //     } else {
+            //         pos = [e.clientX + document.documentElement.scrollLeft, e.clientY + document.documentElement.scrollTop];
+            //     }
+            //     if (!mouse.pos) {
+            //         mouse.pos = pos;
+            //         return;
+            //     }
+            //     mouse.angle = Math.atan2(pos[1] - mouse.pos[1], pos[0] - mouse.pos[0]) * 180 / Math.PI;
+            //     mouse.pos = pos;
+
+            // }, 100), false);
+
+            // var nav_onMouseLeave = function(e)
+            // {
+            //     app.closeNav();
+            // };
+
+            // addEvent($header, 'mouseenter', function(e)
+            // {
+            //     if (nav_open || typeof mouse.angle !== 'number') {
+            //         return;
+            //     }
+            //     if (e.hasOwnProperty('pageX')) {
+            //         pos = [e.pageX, e.pageY];
+            //     } else {
+            //         pos = [e.clientX + document.documentElement.scrollLeft, e.clientY + document.documentElement.scrollTop];
+            //     }
+            //     if (mouse.angle > -180 && mouse.angle < -90) {
+            //         if (pos[0] < 160) {
+            //             nav_open = true;
+            //             nav_mouse = false;
+            //             setTimeout(function() {
+            //                 addEvent($nav, 'mouseleave', nav_onMouseLeave, false);
+            //             }, 150);
+            //             app.openNav();
+            //         }
+            //     }
+            // },
+            // false);
+
+            // var $nav     = document.getElementById('nav'),
+            //     $overlay = document.getElementById('overlay'),
+            //     $main    = document.getElementsByTagName('main')[0];
+
+            // app.openNav = function()
+            // {
+            //     nav_open = true;
+            //     addClass($nav, 'nav--active');
+            //     removeClass($nav, 'nav--closed');
+            //     removeClass($nav, 'nav--hidden');
+            //     addClass($overlay, 'overlay--active');
+            // };
+
+            // app.closeNav = function()
+            // {
+            //     mouse = { };
+            //     nav_open = false;
+            //     addClass($nav, 'nav--closed');
+            //     removeClass($nav, 'nav--active');
+            //     removeClass($overlay, 'overlay--active');
+            //     removeEvent($nav, 'mouseleave', nav_onMouseLeave, false);
+            //     setTimeout(function() {
+            //         addClass($nav, 'nav--hidden');
+            //     }, 300);
+            // };
+
+            // addEvent(document.getElementById('nav-open'), 'click', function(e)
+            // {
+            //     if (e.preventDefault) e.preventDefault(); else e.returnValue = false;
+            //     app.openNav();
+            // },
+            // false);
+
+            // addEvent(document.getElementById('nav-close'), 'click', function(e)
+            // {
+            //     if (e.preventDefault) e.preventDefault(); else e.returnValue = false;
+            //     app.closeNav();
+            // },
+            // false);
+
+            // addEvent(document.getElementById('overlay'), 'click', app.closeNav, false);
+
+            // addEvent(window, 'keydown', function(e)
+            // {
+            //     if (e.which === 27) {
+            //         setTimeout(function() {
+            //             if (nav_open) app.closeNav();
+            //         }, 50);
+            //     }
+            // }, false);
 
             app.iscroll();
 
@@ -357,6 +387,14 @@ if (/dbushell\.com/.test(window.location.hostname))
     })();
 
 })(window, window.document);
+
+/*!
+ * headroom.js v0.7.0 - Give your page some headroom. Hide your header until you need it
+ * Copyright (c) 2014 Nick Williams - http://wicky.nillia.ms/headroom.js
+ * License: MIT
+ */
+
+!function(a,b){"use strict";function c(a){this.callback=a,this.ticking=!1}function d(b){return b&&"undefined"!=typeof a&&(b===a||b.nodeType)}function e(a){if(arguments.length<=0)throw new Error("Missing arguments in extend function");var b,c,f=a||{};for(c=1;c<arguments.length;c++){var g=arguments[c]||{};for(b in g)f[b]="object"!=typeof f[b]||d(f[b])?f[b]||g[b]:e(f[b],g[b])}return f}function f(a){return a===Object(a)?a:{down:a,up:a}}function g(a,b){b=e(b,g.options),this.lastKnownScrollY=0,this.elem=a,this.debouncer=new c(this.update.bind(this)),this.tolerance=f(b.tolerance),this.classes=b.classes,this.offset=b.offset,this.scroller=b.scroller,this.initialised=!1,this.onPin=b.onPin,this.onUnpin=b.onUnpin,this.onTop=b.onTop,this.onNotTop=b.onNotTop}var h={bind:!!function(){}.bind,classList:"classList"in b.documentElement,rAF:!!(a.requestAnimationFrame||a.webkitRequestAnimationFrame||a.mozRequestAnimationFrame)};a.requestAnimationFrame=a.requestAnimationFrame||a.webkitRequestAnimationFrame||a.mozRequestAnimationFrame,c.prototype={constructor:c,update:function(){this.callback&&this.callback(),this.ticking=!1},requestTick:function(){this.ticking||(requestAnimationFrame(this.rafCallback||(this.rafCallback=this.update.bind(this))),this.ticking=!0)},handleEvent:function(){this.requestTick()}},g.prototype={constructor:g,init:function(){return g.cutsTheMustard?(this.elem.classList.add(this.classes.initial),setTimeout(this.attachEvent.bind(this),100),this):void 0},destroy:function(){var a=this.classes;this.initialised=!1,this.elem.classList.remove(a.unpinned,a.pinned,a.top,a.initial),this.scroller.removeEventListener("scroll",this.debouncer,!1)},attachEvent:function(){this.initialised||(this.lastKnownScrollY=this.getScrollY(),this.initialised=!0,this.scroller.addEventListener("scroll",this.debouncer,!1),this.debouncer.handleEvent())},unpin:function(){var a=this.elem.classList,b=this.classes;(a.contains(b.pinned)||!a.contains(b.unpinned))&&(a.add(b.unpinned),a.remove(b.pinned),this.onUnpin&&this.onUnpin.call(this))},pin:function(){var a=this.elem.classList,b=this.classes;a.contains(b.unpinned)&&(a.remove(b.unpinned),a.add(b.pinned),this.onPin&&this.onPin.call(this))},top:function(){var a=this.elem.classList,b=this.classes;a.contains(b.top)||(a.add(b.top),a.remove(b.notTop),this.onTop&&this.onTop.call(this))},notTop:function(){var a=this.elem.classList,b=this.classes;a.contains(b.notTop)||(a.add(b.notTop),a.remove(b.top),this.onNotTop&&this.onNotTop.call(this))},getScrollY:function(){return void 0!==this.scroller.pageYOffset?this.scroller.pageYOffset:void 0!==this.scroller.scrollTop?this.scroller.scrollTop:(b.documentElement||b.body.parentNode||b.body).scrollTop},getViewportHeight:function(){return a.innerHeight||b.documentElement.clientHeight||b.body.clientHeight},getDocumentHeight:function(){var a=b.body,c=b.documentElement;return Math.max(a.scrollHeight,c.scrollHeight,a.offsetHeight,c.offsetHeight,a.clientHeight,c.clientHeight)},getElementHeight:function(a){return Math.max(a.scrollHeight,a.offsetHeight,a.clientHeight)},getScrollerHeight:function(){return this.scroller===a||this.scroller===b.body?this.getDocumentHeight():this.getElementHeight(this.scroller)},isOutOfBounds:function(a){var b=0>a,c=a+this.getViewportHeight()>this.getScrollerHeight();return b||c},toleranceExceeded:function(a,b){return Math.abs(a-this.lastKnownScrollY)>=this.tolerance[b]},shouldUnpin:function(a,b){var c=a>this.lastKnownScrollY,d=a>=this.offset;return c&&d&&b},shouldPin:function(a,b){var c=a<this.lastKnownScrollY,d=a<=this.offset;return c&&b||d},update:function(){var a=this.getScrollY(),b=a>this.lastKnownScrollY?"down":"up",c=this.toleranceExceeded(a,b);this.isOutOfBounds(a)||(a<=this.offset?this.top():this.notTop(),this.shouldUnpin(a,c)?this.unpin():this.shouldPin(a,c)&&this.pin(),this.lastKnownScrollY=a)}},g.options={tolerance:{up:0,down:0},offset:0,scroller:a,classes:{pinned:"headroom--pinned",unpinned:"headroom--unpinned",top:"headroom--top",notTop:"headroom--not-top",initial:"headroom"}},g.cutsTheMustard="undefined"!=typeof h&&h.rAF&&h.bind&&h.classList,a.Headroom=g}(window,document);
 
 /*!
  * based on FitVids | https://github.com/davatron5000/FitVids.js
